14eee73ed75f0b9f994261ce3634d0db6ddc86c2
kairikadev <80520850+kairikadev@users.noreply.github.com>
2021-09-16 15:45:51 +0200

    Added axios and API integration

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,13 +1,22 @@
 import React, {useState} from "react";
 import "./Dictionary.css";
 import { GiMagnifyingGlass } from 'react-icons/gi';
+import axios from "axios";
+
 
 export default function Dictionary(){
     let [keyword, setKeyword] = useState("");
 
+    function handleResponse(response){
+        console.log(response.data);
+    }
+
     function search(event){
         event.preventDefault();
         alert(`Searching ${keyword}`);
+
+        let apiUrl=`https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
+        axios.get(apiUrl).then(handleResponse);
     }
     function handleKeyword(event){
         setKeyword(event.target.value);
