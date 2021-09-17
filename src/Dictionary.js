430285ac052924cb9bc0d715762c934220c66010
kairikadev <80520850+kairikadev@users.noreply.github.com>
2021-09-17 12:31:39 +0200

    added useState

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,4 +1,5 @@
 import React, {useState} from "react";
+import Results from "./Results";
 import "./Dictionary.css";
 import { GiMagnifyingGlass } from 'react-icons/gi';
 import axios from "axios";
@@ -6,9 +7,11 @@ import axios from "axios";
 
 export default function Dictionary(){
     let [keyword, setKeyword] = useState("");
+    let [results, setResults] = useState(null);
 
     function handleResponse(response){
         console.log(response.data);
+        setResults(response.data[0]);
     }
 
     function search(event){
@@ -24,12 +27,13 @@ export default function Dictionary(){
     }
     return(
         <div className="dictionary">
-           <form onSubmit={search}>
-           <input type="search" placeholder="Search" onChange={handleKeyword} classNames="search-field" />
+           <form onSubmit={search} className="mt-3">
+           <input type="search" placeholder="Search..." onChange={handleKeyword} classNames="search-field" />
         <button type="submit" className="search-button">
           <GiMagnifyingGlass />
         </button>
            </form>
+           <Results results={results} />
         </div>
       
     )
