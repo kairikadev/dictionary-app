4e2a75bec2dba09f69ae939464e0f09cc034fb06
kairikadev <80520850+kairikadev@users.noreply.github.com>
2021-09-22 16:36:03 +0200

    Added phonetics

---
+++ src/Phonetics.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Phonetics(props){
+    return(
+        <div className="Phonetics">
+            /{props.phonetic.text}/
+            <br />
+            <a href={props.phonetic.audio} target="_blank" rel="noreferrer">Listen</a>
+        </div>
+    )
+}
