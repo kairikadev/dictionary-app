33d548e9c4cd8d52a92806b7937bf9e91f8e5315
kairikadev <80520850+kairikadev@users.noreply.github.com>
2021-09-29 11:53:01 +0200

    Added Photos component and API

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,5 +1,6 @@
 import React, {useState} from "react";
 import Results from "./Results";
+import Photos from "./Photos";
 import "./Dictionary.css";
 import { GiMagnifyingGlass } from 'react-icons/gi';
 import axios from "axios";
@@ -9,17 +10,25 @@ export default function Dictionary(props){
     let [keyword, setKeyword] = useState(props.defaultWord);
     let [results, setResults] = useState(null);
     let [loaded, setLoaded] = useState(false);
+    let [photos, setPhotos] = useState(null);
 
     function handleResponse(response){
-        console.log(response.data);
+        
         setResults(response.data[0]);
+    }
+    function handlePexelsResponse(response){
+        console.log(response)
+        setPhotos(response.data.photos);
     }
 
     function search(){
         let apiUrl=`https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
         axios.get(apiUrl).then(handleResponse);
 
-        let pexelsApiKey = `563492ad6f917000010000011069b06c748a4dee843ca49fe911797e`;
+        let pexelsApiKey = "563492ad6f917000010000011069b06c748a4dee843ca49fe911797e";
+        let pexelsApiUrl = `https://api.pexels.com/v1/search?query=${keyword}&per_page=9`;
+        let headers = {Authorization:`Bearer ${pexelsApiKey}`};
+        axios.get(pexelsApiUrl, {headers: headers}).then(handlePexelsResponse);
     }
 
     function handleSubmit(event){
@@ -52,6 +61,7 @@ export default function Dictionary(props){
                    suggested words: sunset, wine, plants etc.....
                </div>
                <Results results={results} />
+               <Photos photos={photos} />
             </div>
           
         )
